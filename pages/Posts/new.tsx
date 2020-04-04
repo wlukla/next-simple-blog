4777f79eeea5617e6e2624a94eab0435f78175d7
wlukla <vladislav.danilochkin@gmail.com>
2020-04-04 09:48:20 +0300

    feat: add route for new post creation

---
+++ pages/Posts/new.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { NextPage } from 'next';
+
+import Layout from '../../components/Layout';
+
+const CreatePost: NextPage = () => {
+  return (
+    <Layout>
+      <h1>CreatePost</h1>
+    </Layout>
+  );
+};
+
+export default CreatePost;
